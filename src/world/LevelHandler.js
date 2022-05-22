758e172ff8f98c9f02ded8629c9e3c5319f1bbce
bupledox <bupledox@gmail.com>
2022-05-22 13:48:37 +0100

    fix the levels lol

--- src/world/LevelHandler.js
+++ src/world/LevelHandler.js
@@ -20,10 +20,10 @@ export default class LevelFactory {
     constructor(toChangeLevel) {
         // basic level, mwh,w,h,func,nextLevelindex(in this list),source
         this.Levels = [
-            (mwh, w, h, toChangeLevel) => new BasicLevel(mwh,w,h, toChangeLevel,1,1),
-            (mwh, w, h, toChangeLevel) => new Level2(mwh,w,h, toChangeLevel,2),
-            (mwh, w, h, toChangeLevel) => new BasicLevel(mwh,w,h, toChangeLevel,3,0),
-            (mwh, w, h, toChangeLevel) => new Level1(mwh,w,h, toChangeLevel),
+            (mwh, w, h, toChangeLevel) => new Level2(mwh,w,h, toChangeLevel,1), //Tutorial
+            (mwh, w, h, toChangeLevel) => new BasicLevel(mwh,w,h, toChangeLevel,2,0), //another tutorial
+            (mwh, w, h, toChangeLevel) => new Level1(mwh,w,h, toChangeLevel,3), //First one i made
+            (mwh, w, h, toChangeLevel) => new BasicLevel(mwh,w,h, toChangeLevel,0,1), //tree/impossible
         ];
     }
     getLevel(index) {
--- src/world/Levels/Level1.js
+++ src/world/Levels/Level1.js
@@ -58,7 +58,8 @@ export class Level1 extends LevelBase {
     setupEvents(){
           
         this.MatterWorldHandler.registerPlayerCollisionEvent(this.goals[0],()=>{
-            this.goals[0].render.fillStyle = Colors.Obsticals;
+            //this.goals[0].render.fillStyle = Colors.Obsticals;
+            this.changeLevelFunc(this.targetLevel);
         });
 
     }
