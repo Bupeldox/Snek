0b88d33020a8e0cb05aa653c91736072df12f736
bupledox <bupledox@gmail.com>
2022-05-30 22:50:05 +0100

    black whole mode better

--- src/Player/Worm.js
+++ src/Player/Worm.js
@@ -24,7 +24,7 @@ export default class Snek {
         this.lastPlaced = Date.now();
         this.isMoving = false;
         this.isReversing = false;
-        this.SingularityCounter = 2;
+        this.SingularityCounter = 20;
     }
     addSegment(p, width) {
 
