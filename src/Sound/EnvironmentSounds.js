578fc193816736cc937152d4363886233c5787f5
bupledox <bupledox@gmail.com>
2022-09-06 20:36:26 +0100

    switch to howler and remove slither sound

--- src/Sound/EnvironmentSounds.js
+++ src/Sound/EnvironmentSounds.js
@@ -1,45 +1,62 @@
 
+import { Howl } from "howler";
 import { MatterCollisionHandler } from "../MatterHandler";
-import { SoundHelper} from "./SoundHelper.js";
+import { SoundHelper } from "./SoundHelper.js";
 import { Environment } from "./SoundSrc.js";
+
 
 
 export class EnvironmentSounds{
     constructor(){
         this.collisionDetector = new MatterCollisionHandler();
-        this.createCollisionSound = ()=>new SoundHelper(Environment.hit,false,0.2);
+        this.createCollisionSound = ()=>new Howl({src:Environment.hit,loop:false,volume:0.2});
         this.sounds = {
             collision:[this.createCollisionSound()],
-            levelEnd:new SoundHelper(Environment.finishLevel,false,0.05),
-            blackHole:new SoundHelper(Environment.blackHole,false,0.5)
+            levelEnd:new Howl({src:Environment.finishLevel,loop:false,volume:0.05}),
+            blackHole:new Howl({src:Environment.blackHole,loop:false,volume:0.5})
         }
         window.hit = 5;
-        
+        this.maxCollisionSounds = 5;
         this.playedCollisions = [];
+
+        //deal with startup collision sounds
+        this.shouldDoCollisionSounds = false;
+        setTimeout(()=>{
+            this.shouldDoCollisionSounds = true;
+        },100)
+
+
     }
     update(){
-        var collisions = this.collisionDetector.getPairs();
+        if(this.shouldDoCollisionSounds){
+            var collisions = this.collisionDetector.getPairs();
 
-        var hardCollisions = collisions.filter(i=>i.collision.depth>7)
-        var freeCollisionSounds = this.sounds.collision.filter(i=>!i.playing);
-        var index = 0;
-        for(var i=0;i<hardCollisions.length;i++){
-            var col = hardCollisions[i];
-            if(this.playedCollisions.includes(col.id)){
-                continue;
+            var hardCollisions = collisions.filter(i=>i.collision.depth>7)
+            var freeCollisionSounds = this.sounds.collision.filter(i=>!i.playing);
+            var index = 0;
+            for(var i=0;i<hardCollisions.length;i++){
+                var col = hardCollisions[i];
+                if(this.playedCollisions.includes(col.id)){
+                    continue;
+                }
+                if(freeCollisionSounds.length>index){
+                    freeCollisionSounds[index].play();
+                    index++;
+                }else{
+                    if(this.maxCollisionSounds > index){
+
+                        var colSound = this.createCollisionSound();
+                        
+                        if(this.shouldDoCollisionSounds){
+                            colSound.play();
+                        }
+                        this.sounds.collision.push(colSound);
+                        console.log(this.sounds.collision.length)
+                    }
+                }
+                this.playedCollisions.push(col.id)
             }
-            if(freeCollisionSounds.length>index){
-                freeCollisionSounds[index].play();
-                index++;
-            }else{
-                var colSound = this.createCollisionSound();
-                colSound.play();
-                this.sounds.collision.push(colSound);
-                console.log(this.sounds.collision.length)
-            }
-            this.playedCollisions.push(col.id)
         }
-        
         
 
         
--- src/Sound/SnekSounds.js
+++ src/Sound/SnekSounds.js
@@ -4,6 +4,9 @@ import { SoundHelper, ProportionalSoundHelper} from "./SoundHelper.js";
 import Vec2 from "../Utilities/vec2";
 import { Environment, Snake } from "./SoundSrc.js";
 
+import {Howl, Howler} from 'howler';
+
+
 var onBeat = ()=>{};
 setInterval(()=>{
     onBeat();
@@ -12,68 +15,27 @@ setInterval(()=>{
 export class SnekSounds {
     constructor(snek){
         this.sounds = {
-            slitherSound : new ProportionalSoundHelper(Snake.move,true,0.1,0.51),
-            hissSound : new SoundHelper(Snake.hiss,true,0.1),
-            fallingSound : new SoundHelper(Environment.falling,false,0.4),
+           
+            hissSound : new Howl({ src:Snake.hiss,loop:true,volume:0.1}),
+            fallingSound : new Howl({ src:Environment.falling,loop:false,volume:0.4}),
         }
         this.snek = snek;
         this.snekIds = snek.objects.map(i=>i.id);
         this.collisionDetector = new MatterCollisionHandler();
-        window.sliterFactor = 5;
 
-        this.sounds.slitherSound.b = false;
-
-
-        onBeat = ()=>{
-            this.sounds.slitherSound.destroy();
-            if(this.sounds.slitherSound.b){
-                this.sounds.slitherSound.sound.src = Snake.move
-            }else{
-                this.sounds.slitherSound.sound.src = Snake.moveb
-            }
-            this.sounds.slitherSound.b = !this.sounds.slitherSound.b; 
-        };
 
     }
     update(){
-        var playingFallSound = false;
+
         //sliding sound
         var collisions = this.collisionDetector.getPairs().filter(i=>this.snekIds.includes(i.bodyA.id) ||this.snekIds.includes(i.bodyB.id));
-        var playSliterSound = false;
-        if(collisions.length > 0){
-            var totalNormals = new Vec2(0,0);
-            for(var i=0;i<collisions.length;i++){
-                var pair = collisions[i];
-                
-
-                var bodyAMomentum = new Vec2(pair.bodyA.velocity);
-                var bodyBMomentum = new Vec2(pair.bodyB.velocity);
-                var relativeMomentum = bodyAMomentum.sub(bodyBMomentum);
-                var force = relativeMomentum.magnitude();
-                
-
-                var flip = 1;
-                if(this.snekIds.includes(pair.bodyB.id)){
-                    flip = -1;
-                }
-                totalNormals = totalNormals.add(new Vec2(pair.collision.normal).times(flip).times(force));
-            }
-            var tdot = totalNormals.normalised().dot(new Vec2(0,-1));
-            var totalForce = totalNormals.magnitude();
-            if(tdot<0.97 && totalForce>2){
-                playSliterSound = true;
-                //var vol = Math.min(1,Math.pow(totalForce/window.sliterFactor,2));
-                //this.sounds.slitherSound.volume = vol;
-            }
-        }else{
+        if(collisions.length == 0){
             //falling
-            this.sounds.fallingSound.play();
-            playingFallSound = true;
-        }
-        if(playSliterSound){
-            this.sounds.slitherSound.play();
+            if(!this.sounds.fallingSound.playing()){
+                this.sounds.fallingSound.play();
+            }
         }else{
-            this.sounds.slitherSound.stop();
+            this.sounds.fallingSound.stop();
         }
 
         //Hiss Sound
@@ -86,21 +48,13 @@ export class SnekSounds {
             this.wasPreviouslyMoving = false;
         }
 
-        if(!playingFallSound){
-            this.sounds.fallingSound.stop();
-        }
-
-        for(var i in this.sounds){
-            var sound = this.sounds[i];
-            sound.update();
-
-        }
 
     }
     destroy(){
         for(var i in this.sounds){
-            var sound = this.sounds[i]
-            sound.destroy();
+            var sound = this.sounds[i];
+            sound.stop();
+
         }
         clearInterval(this.beatInterval);
     }
